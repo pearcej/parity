ced7b12df921ea7af028415300be5507877ef826
pearcej <jan_pearce@berea.edu>
2019-09-20 06:12:18 -0400

    upgrade to claass; upgrade VS

--- parity/parity-main.cpp
+++ parity/parity-main.cpp
@@ -1,8 +1,7 @@
-/** L03: Parity
-FIXME: Add names here // CHANGE THIS!!
-FIXME: username(s):  // CHANGE THIS!!
-Purpose: To use arrays, functions,
-and learn some things about binary.
+/** L05: Parity
+FIXME: Add names here // CHANGE THIS
+FIXME: username(s):  // CHANGE THIS
+Purpose: To use arrays, classes, and to learn some things about binary.
 
 Acknowledgements:
 Modified from original code written by Dr. Jan Pearce
@@ -12,64 +11,83 @@ FIXME: Add all other resources used // CHANGE THIS!!
 #include <iostream>
 using namespace std;
 
-void get_byte(char byte[]) {
-	char readbit;
+class Parity {
+public:
+	Parity() {} //default constructor sets all member variables.
 
-	for (int i = 0; i < 7; i++) {
-		cout << "Please enter bit " << i << ": ";
-		cin >> readbit;
-		//FIXME: Put code here to put bit into correct place in byte array
-	}
-
-	cout << endl;
-	return;
-}
-
-void print_bitstring(const char byte[]) { //This is pass by const array, making the array immutable by the function.
-	for (int i = 0; i < 8; i++) {
-		cout << byte[i];
-	}
-	cout << endl;
-	return;
-}
-
-
-//FIXME: Create a new function that when called will print out ASCII art as a good-bye.
-
-
-bool if_binary_evenparity(char byte[8]) { 
-	/**
-	This function takes an 8-bit byte as input,
-	if the first 7 bits consist solely of 0s and 1s
-	then it adds an even parity bit in the last place in the array
-	and returns true.
-	It returns false otherwise */
-
-	//FIXME: Add code here
-
-	return(true); // obviously this should not always return true
-}
-
-
-
-int main( ){
-
-	char readchar ='Y';
-	char byte[8];
-
-	cout << "Welcome to the even parity byte maker!\n" << endl; //FIXME: Feel free to fix this!
-
-	while (readchar == 'Y' || readchar == 'y') {
-		get_byte(byte); //This sends the array via "pass by array" which allows it to be changed like pass by reference.
-		if (if_binary_evenparity(byte)==true){
-			cout << "Currently, the byte has junk in it:" << endl; // FIXME
-			print_bitstring(byte); 
+	Parity(bool tempbyte[8]) {
+		for (int i = 0; i < bytelen; i++) {
+			byte[i] = tempbyte[i];
 		}
-		cout << "\nDo you want to get another string? (If yes, enter y): ";
-		cin >> readchar;
 	}
 
-	//FIXME: Call your ascii printer function here.
+	void set_byte() { //mutator method sets byte from beyboard
+
+		bool readbit;
+
+		for (int i = 0; i < bytelen; i++) {
+			cout << "Please enter bit " << i << ": ";
+			cin >> readbit;
+			byte[i] = readbit;
+		}
+
+		cout << endl;
+		return;
+	}
+
+	void show_byte() {
+
+		for (int i = 0; i < bytelen; i++) {
+			cout << byte[i];
+		}
+		return;
+	}
+
+	bool if_binary_evenparity() {
+		/**
+		This function takes an 8-bit byte as input,
+		if the first 7 bits consist solely of 0s and 1s
+		then it adds an even parity bit in the last place in the array
+		and returns true.
+		It returns false otherwise */
+
+		//FIXME: Add code here
+
+		return(true); // obviously this should not always return true
+	}
+
+
+
+private:
+	bool byte[8] = { false }; //This initializes all of the bits
+	const int bytelen = 8;
+};
+
+
+
+
+
+
+
+int main() {
+
+	bool truebyte[8] = { true, true, true, true, true, true, true, true }; // this style of initalization only works at compile time
+	int readchar = 'Y';
+
+	cout << "Welcome to the parity class!\n" << endl; //FIXME: Feel free to change this!
+
+	Parity testbyte;
+	Parity mybyte(truebyte);  // uses contructor that sets byte using parameter
+
+	testbyte.set_byte();
+	cout << "Here is the byte you entered: ";
+	testbyte.show_byte();
+	cout << endl;
+
+	cout << "Here is anthor byte: ";
+	mybyte.show_byte();
+	cout << endl;
+	//FIXME: place code to fully test your Parity class
 
 	cin >> readchar; //This is to keep screen open in some situations.
 
